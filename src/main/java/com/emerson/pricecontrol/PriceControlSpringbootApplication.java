65a62015b208a33b3d11dc8c3ad8f967edc3913d
emerson borges dell-win <emerson2borges@gmail.com>
2024-12-13 16:15:01 -0300

    primeiro commmit. iniciando projeto

---
+++ src/main/java/com/emerson/pricecontrol/PriceControlSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.emerson.pricecontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceControlSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceControlSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emerson/pricecontrol/PriceControlSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emerson.pricecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceControlSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
