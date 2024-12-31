dd7f2239a788fe52981e2976a63a346a9336ff8e
emerson dsk <emerson2borges@gmail.com>
2024-12-31 10:59:10 -0300

    fix: corrige teste basico

--- src/test/java/com/emerson/pricecontrol/PriceControlSpringbootApplicationTests.java
+++ src/test/java/com/emerson/pricecontrol/PriceControlSpringbootApplicationTests.java
@@ -6,14 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PriceControlSpringbootApplicationTests {
 
-<<<<<<< HEAD
-	@Test
-	void contextLoads() {
-	}
-=======
-    @Test
-    void contextLoads() {
-    }
->>>>>>> 65a62015b208a33b3d11dc8c3ad8f967edc3913d
+//	@Test
+//	void contextLoads() {
+//	}
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
