69d0838480e8af78ef81a18c80f74509b2c37af6
emerson dsk <emerson2borges@gmail.com>
2025-01-06 16:18:57 -0300

    fix(brand): corrige editar eliminando id por url

--- src/main/java/com/emerson/pricecontrol/controller/BrandController.java
+++ src/main/java/com/emerson/pricecontrol/controller/BrandController.java
@@ -4,6 +4,7 @@ import com.emerson.pricecontrol.dto.BrandDTO;
 import com.emerson.pricecontrol.dto.ResponseDTO;
 import com.emerson.pricecontrol.entity.Brand;
 import com.emerson.pricecontrol.entity.Product;
+import com.emerson.pricecontrol.entity.Supermarket;
 import com.emerson.pricecontrol.repository.BrandRepository;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
@@ -46,18 +47,29 @@ public class BrandController {
             }
         }
 
-    @PutMapping("/{id}")
-    public ResponseEntity put(@PathVariable Long id, @Valid @RequestBody BrandDTO brandDTO) {
+    @PutMapping()
+    public ResponseEntity put(@Valid @RequestBody BrandDTO brandDTO) {
         try {
-            Optional<Brand> optionalBrand = brandRepository.findById(id);
-            if (optionalBrand.isPresent()) {
-                Brand existingBrand = optionalBrand.get();
-                mapper.map(brandDTO, existingBrand);
-                brandRepository.save(existingBrand);
-                return new ResponseEntity<>(existingBrand, HttpStatus.OK);
-            } else {
+            // Verifica se o ID está presente no corpo da requisicao
+            if (brandDTO.getId() == null) {
+                return new ResponseEntity<>(new ResponseDTO("ID da marca não fornecida"), HttpStatus.BAD_REQUEST);
+            }
+
+            // Verifica se o supermercado existe
+            Optional<Brand> optionalBrand = brandRepository.findById(brandDTO.getId());
+            if (optionalBrand.isEmpty()) {
                 return new ResponseEntity<>(new ResponseDTO("Marca não encontrada"), HttpStatus.NOT_FOUND);
             }
+
+
+            // Mapeia as alterações do DTO para a entidade existente
+            Brand existingBrand = optionalBrand.get();
+            mapper.map(brandDTO, existingBrand);
+
+            // Salva as alterações no repositório
+            Brand updatedBrand = brandRepository.save(existingBrand);
+            return new ResponseEntity<>(new ResponseDTO("Marca atualizada com sucesso!"), HttpStatus.OK);
+
         } catch (Exception error) {
             return new ResponseEntity<>(new ResponseDTO(error.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
@@ -73,8 +85,8 @@ public class BrandController {
 
             brandRepository.deleteById(id);
             return new ResponseEntity<>(new ResponseDTO("Marca removida com sucesso!"), HttpStatus.OK);
-        } catch (Exception error){
-            return new ResponseEntity<>(new ResponseDTO(error.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception ex){
+            return new ResponseEntity<>(new ResponseDTO(ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- src/main/java/com/emerson/pricecontrol/dto/BrandDTO.java
+++ src/main/java/com/emerson/pricecontrol/dto/BrandDTO.java
@@ -2,17 +2,17 @@ package com.emerson.pricecontrol.dto;
 
 public class BrandDTO {
 
-    private int id;
+    private Long id;
     private String name;
     private String description;
 
     public BrandDTO() {}
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
