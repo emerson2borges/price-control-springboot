af74370f00d46df0ea3a00f79003f4f4d2a5be3e
emerson dsk <emerson2borges@gmail.com>
2025-01-06 15:28:43 -0300

    feat(brand): adiciona put e delete

--- src/main/java/com/emerson/pricecontrol/controller/BrandController.java
+++ src/main/java/com/emerson/pricecontrol/controller/BrandController.java
@@ -1,7 +1,11 @@
 package com.emerson.pricecontrol.controller;
 
+import com.emerson.pricecontrol.dto.BrandDTO;
+import com.emerson.pricecontrol.dto.ResponseDTO;
 import com.emerson.pricecontrol.entity.Brand;
+import com.emerson.pricecontrol.entity.Product;
 import com.emerson.pricecontrol.repository.BrandRepository;
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +13,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.View;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/brand")
@@ -39,5 +45,37 @@ public class BrandController {
 //            return new ResponseEntity<>(new ResponseDTO(error.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
+
+    @PutMapping("/{id}")
+    public ResponseEntity put(@PathVariable Long id, @Valid @RequestBody BrandDTO brandDTO) {
+        try {
+            Optional<Brand> optionalBrand = brandRepository.findById(id);
+            if (optionalBrand.isPresent()) {
+                Brand existingBrand = optionalBrand.get();
+                mapper.map(brandDTO, existingBrand);
+                brandRepository.save(existingBrand);
+                return new ResponseEntity<>(existingBrand, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(new ResponseDTO("Marca não encontrada"), HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception error) {
+            return new ResponseEntity<>(new ResponseDTO(error.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable long id){
+        try {
+            Optional<Brand> optionalBrand = brandRepository.findById(id);
+            if (optionalBrand.isEmpty()) {
+                return new ResponseEntity<>(new ResponseDTO("Maraca não encontrada"), HttpStatus.NOT_FOUND);
+            }
+
+            brandRepository.deleteById(id);
+            return new ResponseEntity<>(new ResponseDTO("Marca removida com sucesso!"), HttpStatus.OK);
+        } catch (Exception error){
+            return new ResponseEntity<>(new ResponseDTO(error.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
 
